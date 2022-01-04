d980f684f61ef36b88b5a26a6feb684453604c16
quynh1210 <quynh1210.nt@gmail.com>
2022-01-04 17:02:21 +0700

    add file hello

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
